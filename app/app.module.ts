6491e49a76831800c86c8ee4ce9ae7f7bb20a2b6
Zlaya <zlaya.31@gmail.com>
2017-07-02 21:53:27 +0200

    pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,7 +12,8 @@ import {
     EventRouteActivator,
     EventListResolver,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    DurationPipe
 } from './events/index';
 
 import { EventsAppComponent } from './events-app.component';
@@ -40,7 +41,8 @@ import { AuthService } from './user/auth.service';
         Error404Component,
         CreateSessionComponent,
         SessionListComponent,
-        CollapsibleWellComponent
+        CollapsibleWellComponent,
+        DurationPipe
     ],
     providers: [
         EventService,
